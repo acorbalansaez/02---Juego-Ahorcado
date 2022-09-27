6d9e252e844995a05d8919ac2c5acbd558108843
acorbalansaez <agustina.lara.cs@gmail.com>
2022-09-27 02:02:52 -0300

    scroll desbloqueado

--- src/juego.js
+++ src/juego.js
@@ -81,9 +81,9 @@ function animarOjos(ojos) {
 }
 
 // PROHIBIR SCROLL
-window.addEventListener('scroll', () => {
-    window.scrollTo(0, 0);
-});
+// window.addEventListener('scroll', () => {
+//     window.scrollTo(0, 0);
+// });
 
 //INICIAR JUEGO
 iniciarJuego();
